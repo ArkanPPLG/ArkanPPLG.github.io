8c5d68fcec463d3d3fb8b7727e5e271b0a42554f
ArkanPPLG <147840950+ArkanPPLG@users.noreply.github.com>
2023-10-14 16:40:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -81,26 +81,119 @@ function updateWaktu() {
 
     const intm = parseInt(menit)
 
-    const sabtu = {
-        1: "Jepang",
-        2: "Jepang",
-        3: "Sejarah",
-        4: "Sejarah",
-        6: "Istirahat",
-        7: "WEB",
-        8: "WEB",
-        9: "Inggris",
-        10: "Inggris"
+    const jamPelajaran = {
+        0: {
+            1: "Libur",
+            2: "Libur",
+            3: "Libur",
+            4: "Libur",
+            5: "Libur",
+            6: "Libur",
+            7: "Libur",
+            8: "Libur",
+            9: "Libur",
+        },
+
+        1: {
+            1: "Upacara",
+            2: "Upacara",
+            3: "PKK",
+            4: "PKK",
+            5: "Istirahat",
+            6: "Bahasa Indonesia", 
+            7: "PABP", 
+            8: "PBO",
+            9: "PBO"
+        },
+
+        2: {
+            1: "PABP",
+            2: "PABP",
+            3: "Bahasa Indonesia",
+            4: "Bahasa Indonesia",
+            5: "Istirahat",
+            6: "MTK",
+            7: "PKK",
+            8: "PKN",
+            9: "PKN"
+        },
+
+        3: {
+            1: "PBO",
+            2: "PBO",
+            3: "Mikrotik",
+            4: "Mikrotik",
+            5: "Istirahat",
+            6: "PKK",
+            7: "PKK",
+            8: "MTK", 
+            9: "MTK"
+        },
+
+        4: {
+            1: "PBO",
+            2: "PBO",
+            3: "Mikrotik",
+            4: "Mikrotik", 
+            5: "Istirahat",
+            6: "DB",
+            7: "DB",
+            8: "DB",
+            9: "DB",
+        },
+
+        5: {
+            1: "WEB",
+            2: "WEB",
+            3: "PJOK",
+            4: "PJOK",
+            5: "Istirahat",
+            6: "Inggris",
+            7: "Inggris"
+        },
+
+        6: {
+            1: "Jepang",
+            2: "Jepang",
+            3: "Sejarah",
+            4: "Sejarah",
+            5: "Istirahat",
+            6: "WEB",
+            7: "WEB",
+            8: "Inggris",
+            9: "Inggris"
+        }
     }
-    
+
     const formattedTime = formatter.format(date2);
     const input = document.getElementById("jam")
     let text = `${week[today]} : ${formattedTime}`
-    if (jam == "10 PM" && intm >= 0 && intm <= 40) {
-        let pel = sabtu[7]
+
+    let data;
+
+    if(jam == "7 AM" && menit >= 0 && menit < 40){
+        data = jamPelajaran[today][1]
+    }else if(jam == "7 AM" && menit >= 40 && menit <= 59 || jam == "8 AM" && menit >= 0 && menit < 20){
+        data = jamPelajaran[today][2]
+    }else if(jam == "8 AM" && menit >= 20 && menit <= 59){
+        data = jamPelajaran[today][3]
+    }else if(jam == "9 AM" && menit >= 0 && menit < 40){
+        data = jamPelajaran[today][4]
+    }else if(jam == "9 AM" && menit >= 40 && menit <= 59){
+        data = jamPelajaran[today][5]
+    }else if(jam == "10 AM" && menit >= 0 && menit < 40){
+        data = jamPelajaran[today][6]
+    }else if(jam == "10 AM" && menit >= 40 && menit <= 59 || jam == "11 AM" && menit >= 0 && menit < 20){
+        data = jamPelajaran[today][7]
+    }else if(jam == "11 AM" && menit >= 20 && menit < 55){
+        data = jamPelajaran[today][8]
+    }else if(jam == "11 AM" && menit >= 55 && menit <= 59 || jam == "12 AM" && menit >= 0 && menit < 30){
+        data = jamPelajaran[today][9]
+    }else{
+        data = "tidak ada jam pelajaran"
     }
-    
-    input.innerHTML = text
+
+    input.innerHTML = `${text} <br>jam pelajaran : ${data}`
 }
-setInterval(updateWaktu,1000)
+setInterval(updateWaktu, 1000)
 updateWaktu()
